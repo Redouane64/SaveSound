e3234c6fa4b21300d46168822062311ee10e0371
Redouane Sobaihi <s-redouane@outlook.com>
2019-09-25 12:50:56 +0700

    Add splash screen
    Signed-off-by: Redouane Sobaihi <s-redouane@outlook.com>

--- app/src/main/java/com/github/redouane64/savesound/MainActivity.kt
+++ app/src/main/java/com/github/redouane64/savesound/MainActivity.kt
@@ -2,11 +2,19 @@ package com.github.redouane64.savesound
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
 
+    val splashScreen: Long = 2000; // milliseconds.
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /* Pause thread for 2 seconds to display splash screen. */
+        Thread.sleep(splashScreen)
+
+        theme.applyStyle(R.style.AppTheme, true)
         setContentView(R.layout.activity_main)
     }
 }
